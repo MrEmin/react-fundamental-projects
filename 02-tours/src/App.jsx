137170f47cfc8ea6a69a8cfca7973fb28adbe9f2
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-31 21:56:31 +0300

    npm install and created component

--- 02-tours/src/App.jsx
+++ 02-tours/src/App.jsx
@@ -1,6 +1,6 @@
-const url = 'https://course-api.com/react-tours-project';
+const url = 'https://course-api.com/react-tours-project'
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
-};
-export default App;
+  return <h2>Tours Starter</h2>
+}
+export default App
