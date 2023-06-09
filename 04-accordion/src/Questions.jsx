fbff922b0326dd34229be6725eef72925e56b8f1
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-09 21:09:44 +0300

    my solution

---
+++ 04-accordion/src/Questions.jsx
@@ -0,0 +1,12 @@
+import SingleQuestion from './SingleQuestion'
+const Questions = ({ questions }) => {
+  return (
+    <section className='container'>
+      <h1>Questions</h1>
+      {questions.map((question) => (
+        <SingleQuestion {...question} key={question.id} />
+      ))}
+    </section>
+  )
+}
+export default Questions
