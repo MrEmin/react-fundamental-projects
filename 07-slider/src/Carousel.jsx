8e4c3accd9a000f9156b2fdcdef4df2e0bf1afa5
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-10 19:53:13 +0300

    Current Item and Prev/Next

--- 07-slider/src/Carousel.jsx
+++ 07-slider/src/Carousel.jsx
@@ -5,9 +5,20 @@ import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 const Carousel = () => {
   const [people, setPeople] = useState(list)
+  const [currentPerson, setCurrentPerson] = useState(0)
 
-  const prevSlide = () => {}
-  const nextSlide = () => {}
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length
+      return result
+    })
+  }
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length
+      return result
+    })
+  }
 
   return (
     <section className='slider-container'>
@@ -17,7 +28,11 @@ const Carousel = () => {
           <article
             className='slide'
             key={id}
-            style={{ transform: `translateX(${100 * personIndex}%)` }}
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity: personIndex === currentPerson ? '1' : '0',
+              visibility: personIndex === currentPerson ? 'visible' : 'hidden',
+            }}
           >
             <img src={image} alt={name} className='person-img' />
             <h5 className='name'>{name}</h5>
