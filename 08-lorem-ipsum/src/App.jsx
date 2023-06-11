749a45b19a53c44750b923c2b073742c05730f94
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-11 23:14:55 +0300

    teacher's solution

--- 08-lorem-ipsum/src/App.jsx
+++ 08-lorem-ipsum/src/App.jsx
@@ -6,41 +6,36 @@ const App = () => {
   const [count, setCount] = useState(1)
   const [text, setText] = useState([])
 
-  const handleChange = (e) => {
-    const input = e.target.value
-    if (input > 8 || input < 1) {
-      return
-    }
-    setCount(input)
-  }
-
   const handleSubmit = (e) => {
     e.preventDefault()
-    const newParagraph = data.slice(0, count)
-    setText(newParagraph)
+    setText(data.slice(0, count))
   }
 
   return (
-    <div className='section-center'>
+    <section className='section-center'>
       <h4>TIRED OF BORING LOREM IPSUM?</h4>
       <form className='lorem-form' onSubmit={handleSubmit}>
-        <label htmlFor=''>Paragraphs:</label>
-        <input type='number' value={count} onChange={handleChange} />
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          min='1'
+          max='8'
+          step='1'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
         <button type='submit' className='btn'>
-          submit
+          generate
         </button>
       </form>
       <article className='lorem-text'>
         {text.map((paragraph) => {
-          const id = nanoid()
-          return (
-            <p key={id} className='text'>
-              {paragraph}
-            </p>
-          )
+          return <p key={nanoid()}>{paragraph}</p>
         })}
       </article>
-    </div>
+    </section>
   )
 }
 export default App
