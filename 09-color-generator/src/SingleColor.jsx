edd950f2379272b64bb5cb7c4833bc4b4471bc1d
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-12 15:15:04 +0300

    Color Generator - Clipboard API

--- 09-color-generator/src/SingleColor.jsx
+++ 09-color-generator/src/SingleColor.jsx
@@ -1,11 +1,26 @@
+import { toast } from 'react-toastify'
+
 const SingleColor = ({ color, index, length }) => {
   const { hex, weight } = color
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`)
+        toast.success(`Color copied to clipboard!`)
+      } catch (error) {
+        toast.error(`Failed to copy color to clipboard`)
+      }
+    } else {
+      toast.error('Clipboard access not available')
+    }
+  }
   return (
     <article
       className={index > length ? 'color-light color' : 'color'}
       style={{
         background: `#${hex}`,
       }}
+      onClick={saveToClipboard}
     >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>#{hex}</p>
