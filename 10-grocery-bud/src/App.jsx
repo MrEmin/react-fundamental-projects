95c78914b43ba382a0dc5a709e5832d291baeb92
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-13 12:43:31 +0300

    Grocery Bud - Local Storage

--- 10-grocery-bud/src/App.jsx
+++ 10-grocery-bud/src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Form from './Form'
 import Items from './Items'
 import { nanoid } from 'nanoid'
 import { ToastContainer, toast } from 'react-toastify'
 
+/* const getLocalStorage = () => {
+  let list = localStorage.getItem('list')
+  if (list) {
+    list = JSON.parse(localStorage.getItem('list'))
+    console.log(list)
+  } else {
+    list = []
+  }
+  return list
+} */
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('list') || '[]')
+
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items))
+}
+
 const App = () => {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState(getLocalStorage)
 
   const addItem = (itemName) => {
     const newItem = {
@@ -13,13 +30,15 @@ const App = () => {
       completed: false,
       id: nanoid(),
     }
-
-    setItems([...items, newItem])
+    const newItems = [...items, newItem]
+    setItems(newItems)
+    setLocalStorage(newItems)
   }
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId)
     setItems(newItems)
+    setLocalStorage(newItems)
     toast.success('item deleted from list')
   }
 
