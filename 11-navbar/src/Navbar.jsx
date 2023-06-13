733926588858bcafb27e5a848f850eb982c87431
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-13 18:44:49 +0300

    Navbar - Fixed Setup

--- 11-navbar/src/Navbar.jsx
+++ 11-navbar/src/Navbar.jsx
@@ -4,7 +4,7 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  const [showLinks, setShowLinks] = useState(true)
+  const [showLinks, setShowLinks] = useState(false)
 
   const toggleLinks = () => {
     setShowLinks(!showLinks)
@@ -19,20 +19,22 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className='links-container'>
-            <ul className='links'>
-              {links.map((link) => {
-                const { id, url, text } = link
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        )}
+        <div
+          className={
+            showLinks ? 'links-container show-container' : 'links-container'
+          }
+        >
+          <ul className='links'>
+            {links.map((link) => {
+              const { id, url, text } = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   )
